33e8f233f674402bdfc462ab85ff6edcb9f95648
Richard Hong <rhong24@gmail.com>
2023-08-04 13:35:34 -0500

    adding shadcn/ui input, SearchInput component, useDebounce hook

---
+++ components/SearchInput.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const SearchInput = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+  const name = searchParams.get("name");
+
+  const [value, setValue] = useState<string>(name || "");
+
+  return (
+    <div className="relative">
+      <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+      <Input
+        placeholder="Search..."
+        className="pl-10 bg-primary/10"
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
