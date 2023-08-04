138ad61b55d4506fa1d0e97d1fe288ae00b908db
Richard Hong <rhong24@gmail.com>
2023-08-04 13:44:20 -0500

    implementing debounce within SearchInput along with query-string to change url search params after half second delay of user stopping typing

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import qs from "query-string";
+import { ChangeEventHandler, useEffect, useState } from "react";
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useDebounce } from "@/hooks/useDebounce";
 
 const SearchInput = () => {
   const router = useRouter();
@@ -13,11 +15,38 @@ const SearchInput = () => {
   const name = searchParams.get("name");
 
   const [value, setValue] = useState<string>(name || "");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId,
+    };
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  }, [debouncedValue, router, categoryId]);
 
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
       <Input
+        onChange={onChange}
+        value={value}
         placeholder="Search..."
         className="pl-10 bg-primary/10"
       />
